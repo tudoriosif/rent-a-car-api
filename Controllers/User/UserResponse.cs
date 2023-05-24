1bcb3080798bf0608d75fb0d574833e5bc2b37a4
Tudor Andrei Iosif <tudor.iosif@saltandpepper.co>
2023-05-24 12:18:30 +0300

    first init

---
+++ Controllers/User/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace RentACarAPI.Controllers.User
+{
+    public class UserResponse
+    {
+        public string Message { get; set; }
+
+        public bool isSuccess { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
